af6b93c5b5ee4eb57718d53086837c77e1afb63f
Tyler Schill <tylerjschill@gmail.com>
2025-10-27 20:39:24 -0400

    roles in key.py

--- key.py
+++ key.py
@@ -3,7 +3,9 @@ Key program
 '''
 from scapy.all import *
 from statistics import stdev, sum
+import time
 
+'''
 frame = RadioTap() / Ether(dst="E0:DD:AC:54:59:49", src="b6:0b:8a:51:4d:5e")
 
 def analyze_frame(packet):
@@ -34,3 +36,61 @@ for i in range(rssi_vals):
         new_bit = 0
         key = (key << 1) | new_bit
 
+
+dot11 = Dot11(type=0, subtype=8,
+              addr1="ff:ff:ff:ff:ff:ff",
+              addr2="8a:36:19:76:a6:7e",
+              addr3="8a:36:19:76:a6:7e")
+
+'''
+
+
+
+def process_packet(packet):
+    nonlocal responder
+    nonlocal response
+    if packet.haslayer(Dot11Beacon) and packet.haslayer(Dot11Elt):
+        ssid = packet[Dot11Elt].info.decode(errors="ignore")
+        if ssid == "Ready to Begin":
+            beacon = Dot11Beacon(cap="ESS")
+            essid = Dot11Elt(ID="SSID", info="Ready to Exchange", len=14)
+
+            ack_frame = RadioTap()/dot11/beacon/essid
+            sendp(frame, iface="wlp0s20f0u9", inter=0.1, loop=1, verbose=False)
+            responder = True
+            print("I am the responder")
+
+        if ssid == "Ready to Exchange":
+            response = True
+            print("I am the initiator")
+
+def sniffer(q):
+    sniff(iface="wlp0s20f0u9", prn=process_packet, store=False)
+
+def sender(q):
+    time.sleep(3)
+    if responder == False:
+        beacon = Dot11Beacon(cap="ESS")
+        essid = Dot11Elt(ID="SSID", info="Ready to Begin", len=14)
+        frame = RadioTap()/dot11/beacon/essid
+        
+        while response != True:
+            sendp(frame, iface="wlp0s20f0u9", inter=0.1, verbose=False)
+
+
+responder = False
+response = False
+q = Queue()
+
+thread1 = thread.Thread(target=sniffer, args=(q, ))
+thread2 = thread.Thread(target=sender, args=(q, ))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+
+except KeyboardInterrupt:
+    print("Program stopped")

82990d222c0f561076d2e5d94acf711558fe03da
Tyler Schill <tylerjschill@gmail.com>
2025-10-27 20:49:22 -0400

    roles again

--- key.py
+++ key.py
@@ -2,8 +2,11 @@
 Key program
 '''
 from scapy.all import *
-from statistics import stdev, sum
+from scapy.layers.dot11 import Dot11
+#from statistics import stdev, sum
 import time
+from threading import Thread
+from queue import Queue
 
 '''
 frame = RadioTap() / Ether(dst="E0:DD:AC:54:59:49", src="b6:0b:8a:51:4d:5e")
@@ -25,7 +28,6 @@ for i in range(300):
 # calculate avg and standard deviation
 avg = sum(rssi_vals) / len(rssi_vals)
 std_dev = stdev(rssi_vals)
-
 z_value = 1
 new_bit = 0;
 for i in range(rssi_vals):
@@ -35,20 +37,10 @@ for i in range(rssi_vals):
     else:
         new_bit = 0
         key = (key << 1) | new_bit
-
-
-dot11 = Dot11(type=0, subtype=8,
-              addr1="ff:ff:ff:ff:ff:ff",
-              addr2="8a:36:19:76:a6:7e",
-              addr3="8a:36:19:76:a6:7e")
-
 '''
 
 
-
 def process_packet(packet):
-    nonlocal responder
-    nonlocal response
     if packet.haslayer(Dot11Beacon) and packet.haslayer(Dot11Elt):
         ssid = packet[Dot11Elt].info.decode(errors="ignore")
         if ssid == "Ready to Begin":
@@ -70,6 +62,10 @@ def sniffer(q):
 def sender(q):
     time.sleep(3)
     if responder == False:
+        dot11 = Dot11(type=0, subtype=8,
+              addr1="ff:ff:ff:ff:ff:ff",
+              addr2="8a:36:19:76:a6:7e",
+              addr3="8a:36:19:76:a6:7e")
         beacon = Dot11Beacon(cap="ESS")
         essid = Dot11Elt(ID="SSID", info="Ready to Begin", len=14)
         frame = RadioTap()/dot11/beacon/essid
@@ -82,8 +78,8 @@ responder = False
 response = False
 q = Queue()
 
-thread1 = thread.Thread(target=sniffer, args=(q, ))
-thread2 = thread.Thread(target=sender, args=(q, ))
+thread1 = Thread(target=sniffer, args=(q, ))
+thread2 = Thread(target=sender, args=(q, ))
 
 thread1.start()
 thread2.start()
@@ -91,6 +87,3 @@ thread2.start()
 thread1.join()
 thread2.join()
 
-
-except KeyboardInterrupt:
-    print("Program stopped")
